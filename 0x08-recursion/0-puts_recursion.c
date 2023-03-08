705ee2529286659a8b58feaa398ff5591dc0acce
siyandaMaphitha <siyasi19@gmail.com>
2023-03-08 05:20:06 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
  * Return: 0 Always
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putcahr(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
