374b05c6aae01bed25463bb52495d920a2510cea
siyandaMaphitha <siyasi19@gmail.com>
2023-03-08 06:53:11 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,8 +11,10 @@ void _puts_recursion(char *s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
+		_puts_recursion("First, solve the problem. Then, write the code");
 	}
 
 	else
 		_putchar('\n');
+	return 0;
 }
