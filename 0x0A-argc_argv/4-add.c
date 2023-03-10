35482f78fcededee367edfb89be188758406afb3
siyandaMaphitha <siyasi19@gmail.com>
2023-03-10 11:00:10 -0800

    100-change.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,15 +8,13 @@
 	 *
 	 * Return: Always 0 (Success)
 	 */
-	int check_num(char *str)
-	{
-		/*Declaring variables*/
-		unsigned int count;
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
 
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
+	count = 0;
+	while (count < strlen(str)) /*count string*/
 
 		{
 			if (!isdigit(str[count])) /*check if str there are digit*/
@@ -40,16 +38,12 @@
 	 */
 
 
-	int main(int argc, char *argv[])
-
-
-	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
 
 		count = 1;
