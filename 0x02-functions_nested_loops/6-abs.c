f09d691c43da99eec21d8686d9c9b94bb986997a
siyandaMaphitha <siyasi19@gmail.com>
2023-05-23 09:52:53 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absloute of an interger
+ * @z: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int z)
+{
+	if (z >= 0)
+		return (z);
+	else
+		return (-z);
+}
