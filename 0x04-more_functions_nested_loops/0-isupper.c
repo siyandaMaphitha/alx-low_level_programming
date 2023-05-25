617014a2dea3c603b1e6c2f16b1ec8ccb581c852
siyandaMaphitha <siyasi19@gmail.com>
2023-05-25 05:30:09 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
+ * _isupper - uppercase letters
  * @c: character to check
  *
- * Return:1 if c is uppercase, 0 otherwise
+ * Return: 0 or 1
  */
 int _isupper(int c)
+
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,6 +13,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
+void print_number(int n);
 
-#endif
+
+#endif 
