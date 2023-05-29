4fb35fb74555ceeb0caf934f52ff828bee1effea
siyandaMaphitha <siyasi19@gmail.com>
2023-05-29 10:32:23 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,11 @@ void print_array(int *a, int n)
 
 	for (b = 0; b < n; b++)
 	{
-		if (b == 0)
-			printf("%d, ", a[b]);
-
-	else
-		printf("%d", a[b]);
+		printf("%d, ", a[b]);
+		if (b != (n -1))
+		{
+			printf(", ");
+		}
 	}
-	printf("\n");
+	putchar('\n');
 }
