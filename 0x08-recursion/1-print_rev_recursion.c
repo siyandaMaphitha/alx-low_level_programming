94988ac628fcbdfb41c894932379b42e9e97153d
siyandaMaphitha <siyasi19@gmail.com>
2023-06-07 09:13:22 -0700

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _Print_rev_recursion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  */
 void _print_rev_recursion(char *s)
