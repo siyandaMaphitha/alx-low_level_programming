d694a67fc8e20eaaf40d12e6487974910d499774
siyandaMaphitha <siyasi19@gmail.com>
2023-06-19 05:08:28 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the new file
+ * followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
